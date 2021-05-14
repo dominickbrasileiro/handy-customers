c0146308905ac1a888da45a7319dea8b77c4b578
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-14 17:12:45 -0300

    feat(server): add create-customer use case

---
+++ apps/server/app/controllers/customers_controller.rb
@@ -0,0 +1,17 @@
+class CustomersController < ApplicationController
+  def create
+    @customer = Customer.new(create_customer_params)
+
+    if @customer.save
+      render json: @customer, status: 201
+    else
+      render error: 'Internal Server Error', status: 500
+    end
+  end
+
+  private
+
+  def create_customer_params
+    params.require(:customer).permit(:name, :phone_number, :birth_date)
+  end
+end
