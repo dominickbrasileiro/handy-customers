aee14fe09715cf43b6840dc20ff3f6f7bda9bba2
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-14 17:35:23 -0300

    feat(server): add order model

--- apps/server/db/schema.rb
+++ apps/server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_201858) do
+ActiveRecord::Schema.define(version: 2021_05_14_203311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_05_14_201858) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_05_14_201858) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "customers"
 end
