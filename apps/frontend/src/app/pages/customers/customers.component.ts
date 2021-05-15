dcce411f79f91033a7c3539ebe7ca448a681ee77
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-15 20:03:48 -0300

    feat(frontend): add transformation pipes to customers table

--- apps/frontend/src/app/pages/customers/customers.component.ts
+++ apps/frontend/src/app/pages/customers/customers.component.ts
@@ -9,25 +9,25 @@ export class CustomersComponent {
   customers = [
     {
       name: 'Dominick Brasileiro',
-      birth_date: 'August 26, 2003',
+      birth_date: new Date(2003, 7, 26),
       phone_number: '+5547997573495',
       created_at: new Date()
     },
     {
       name: 'Dominick Brasileiro',
-      birth_date: 'August 26, 2003',
+      birth_date: new Date(2003, 7, 26),
       phone_number: '+5547997573495',
       created_at: new Date()
     },
     {
       name: 'Dominick Brasileiro',
-      birth_date: 'August 26, 2003',
+      birth_date: new Date(2003, 7, 26),
       phone_number: '+5547997573495',
       created_at: new Date()
     },
     {
       name: 'Dominick Brasileiro',
-      birth_date: 'August 26, 2003',
+      birth_date: new Date(2003, 7, 26),
       phone_number: '+5547997573495',
       created_at: new Date()
     }
