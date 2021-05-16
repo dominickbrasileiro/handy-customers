493dad4a4294dff8eb4f5e57058d0e67864149f0
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2021-05-16 00:25:32 -0300

    feat(server): add update-customer use case

--- apps/server/app/controllers/customers_controller.rb
+++ apps/server/app/controllers/customers_controller.rb
@@ -19,9 +19,31 @@ class CustomersController < ApplicationController
     end
   end
 
+  def update
+    customer = Customer.find(params[:customer_id])
+
+    update_params = customer_params
+
+    customer.name = update_params['name'] || customer.name
+    customer.birth_date = update_params['birth_date'] || customer.birth_date
+    customer.phone_number = update_params['phone_number'] || customer.phone_number
+    customer.active = update_params.has_key?(:active) ? update_params['active']  : customer.active
+
+    if not customer.valid?
+      render json: { error: true, message: 'Bad Request' }, status: 400
+      return
+    end
+
+    if customer.save
+      render json: customer, status: 200
+    else
+      render json: { error: true, message:'Internal Server Error' }, status: 500
+    end
+  end
+
   private
 
-  def create_customer_params
-    params.require(:customer).permit(:name, :phone_number, :birth_date)
+  def customer_params
+    params.require(:customer).permit(:name, :phone_number, :birth_date, :active)
   end
 end
--- apps/server/config/routes.rb
+++ apps/server/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/customers", to: "customers#index"
   post "/customers", to: "customers#create"
+  patch "/customers/:customer_id", to: "customers#update"
 
   get "/products", to: "products#index"
   post "/products", to: "products#create"
